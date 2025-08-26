94be0f14ea892b5e3e2cc0e4d0fcb62beab48cfd
Cyber-Buy <Cyber-Buy@DESKTOP-1JH5ML3>
2025-08-26 10:57:48 -0300

    Agregar archivos de proyecto.

---
+++ Service/Models/Categoria.cs
@@ -0,0 +1,15 @@
+﻿namespace Service.Models
+{
+    public class Categoria  
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string categoria { get; set; } = string.Empty;
+        public bool IsDeleted { get; set; } = false;
+
+        public override string ToString()
+        {
+            return Nombre;
+        }
+    }
+}
---
+++ Service/Models/Usuarios.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Models
+{
+    public class Usuarios
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Rol { get; set; } = string.Empty;
+        public bool IsDeleted { get; set; } = false;
+        public string Password { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public override string ToString()
+        {
+             return Nombre;
+        }
+    }
+}
