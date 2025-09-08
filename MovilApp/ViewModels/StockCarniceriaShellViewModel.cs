249c1125d2a6c21d5273eb97660d507cce1dc526
valeinforma <valemacha1805@gmail.com>
2025-09-08 20:18:35 -0300

    Añadir proyecto "MovilApp" y vistas de inicio de sesión
    Se ha añadido un nuevo proyecto llamado "MovilApp" a la solución `StockCarniceria.sln`, junto con su configuración y archivos esenciales como `App.xaml`, `AppShell.xaml`, y `MainPage.xaml`.
    
    Se han implementado vistas para el inicio de sesión y registro, incluyendo `LoginView` y `SignInView`, que permiten a los usuarios gestionar sus cuentas.
    
    Además, se han realizado configuraciones específicas para Android, iOS y Windows, y se han añadido recursos visuales como iconos y colores. También se ha mejorado la lógica de inicio de sesión utilizando Firebase y se han creado archivos de estilo y recursos para la interfaz de usuario. Estos cambios establecen la base para una nueva aplicación multiplataforma utilizando .NET MAUI.

---
+++ MovilApp/ViewModels/StockCarniceriaShellViewModel.cs
@@ -0,0 +1,32 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MovilApp.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovilApp.ViewModels
+{
+    public partial class StockCarniceriaShellViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private bool userIsLogout = true;
+
+        public IRelayCommand LogoutCommand { get; }
+
+        public StockCarniceriaShellViewModel()
+        {
+            LogoutCommand = new RelayCommand(OnLogout);
+        }
+
+        private void OnLogout()
+        {
+            userIsLogout = true;
+            var StockCarniceriaShell = (StockCarniceriaShell)App.Current.MainPage;
+            StockCarniceriaShell.DisableAppAfterLogin();
+        }
+    }
+}
