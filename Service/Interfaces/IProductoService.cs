ceee62b6515b55cd979154e6b752ed0d4898a03d
valeinforma <valemacha1805@gmail.com>
2025-11-14 20:04:55 -0300

    Refactorización de métodos de productos en servicios
    Se eliminó el parámetro `categoriaId` en `GetProductosPorCategoriaAsync` en `Home.razor`, simplificando la lógica de inicialización.
    Se añadieron los métodos `GetProductosPorCategoriaAsync` y `BuscarProductosPorNombreAsync` en la interfaz `IProductoService.cs`, mejorando la coherencia con la implementación.
    En `ProductoService.cs`, se implementaron estos métodos para realizar solicitudes HTTP y manejar errores, asegurando una correcta interacción con la API.

---
+++ Service/Interfaces/IProductoService.cs
@@ -0,0 +1,16 @@
+﻿using Service.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Service.Interfaces
+{
+    public interface IProductoService : IGenericService<Producto>
+    {
+        public Task<List<Producto>?> GetProductosPorCategoriaAsync();
+        public Task<List<Producto>?> BuscarProductosPorNombreAsync();
+    }
+}
---
+++ Service/Services/ProductoService.cs
@@ -0,0 +1,58 @@
+﻿using Service.Interfaces;
+using Service.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Service.Services
+{
+    public class ProductoService : GenericService<Producto>, IProductoService
+    {
+
+        public async Task<List<Producto>?> GetCapacitacionesAbiertasAsync()
+        {
+            var response = await _httpClient.GetAsync($"{_endpoint}/abiertas");
+            var content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Error al obtener los datos: {response.StatusCode}");
+            }
+            return JsonSerializer.Deserialize<List<Producto>>(content, _options);
+        }
+        public async Task<List<Producto>?> GetCapacitacionesFuturasAsync()
+        {
+            var response = await _httpClient.GetAsync($"{_endpoint}/futuras");
+            var content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Error al obtener los datos: {response.StatusCode}");
+            }
+            return JsonSerializer.Deserialize<List<Producto>>(content, _options);
+        }
+        public async Task<List<Producto>?> GetProductosPorCategoriaAsync()
+        {
+            var response = await _httpClient.GetAsync($"{_endpoint}/por-categoria");
+            var content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Error al obtener los datos: {response.StatusCode}");
+            }
+            return JsonSerializer.Deserialize<List<Producto>>(content, _options);
+        }
+
+        public async Task<List<Producto>?> BuscarProductosPorNombreAsync()
+        {
+            var response = await _httpClient.GetAsync($"{_endpoint}/buscar-por-nombre");
+            var content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Error al obtener los datos: {response.StatusCode}");
+            }
+            return JsonSerializer.Deserialize<List<Producto>>(content, _options);
+        }
+    }
+}
